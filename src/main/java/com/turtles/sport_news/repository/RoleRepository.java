92b6d9580af051646c826abb645b956db92d945b
Bohdantrush08 <32901504+Bohdantrush08@users.noreply.github.com>
2019-08-15 17:02:56 +0300

    fixed Privilege and Role repositories

--- src/main/java/com/turtles/sport_news/repository/RoleRepository.java
+++ src/main/java/com/turtles/sport_news/repository/RoleRepository.java
@@ -2,7 +2,9 @@ package com.turtles.sport_news.repository;
 
 import com.turtles.sport_news.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findByName(String name);
 }
