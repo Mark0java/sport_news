efe12f23644898cd57c6aa65c362b82018115ba7
BohdanMytnyk <bbmytnyk@gmail.com>
2019-08-15 15:31:13 +0300

    added UserController, added some methods to UserService and UserRepository

---
+++ src/main/java/com/turtles/sport_news/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.turtles.sport_news.controller;
+
+import com.turtles.sport_news.convertor.UserConvertor;
+import com.turtles.sport_news.dto.SignUpDTO;
+import com.turtles.sport_news.entity.User;
+import com.turtles.sport_news.service.UserService;
+import com.turtles.sport_news.util.ValidationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ValidationService validationService;
+
+    @Autowired
+    private UserConvertor userConvertor;
+
+    @PostMapping("/account")
+    public ResponseEntity<SignUpDTO> createNewAccount(@RequestBody SignUpDTO signUpDTO) {
+        if (!validate(signUpDTO)) {
+            return ResponseEntity.badRequest().build();
+        }
+        User account = userService.createUser(userConvertor.fromSignUpDTO(signUpDTO));
+        return new ResponseEntity<SignUpDTO>(userConvertor.toSignUpDTO(account), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/account/{id}")
+    public ResponseEntity<SignUpDTO> getAccount(@PathVariable(name = "id") Long id) {
+        User account = userService.getUserById(id);
+        return new ResponseEntity<SignUpDTO>(userConvertor.toSignUpDTO(account), HttpStatus.OK);
+    }
+
+    private boolean validate(SignUpDTO signUpDTO) {
+        if (validationService.allExist(signUpDTO.getEmail(), signUpDTO.getPassword(), signUpDTO.getConfirmPassword(), signUpDTO.getEmail(), signUpDTO.getName(), signUpDTO.getSurname())) {
+            return signUpDTO.getConfirmPassword().equals(signUpDTO.getPassword());
+        }
+        return false;
+    }
+}
--- src/main/java/com/turtles/sport_news/repository/UserRepository.java
+++ src/main/java/com/turtles/sport_news/repository/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository  extends JpaRepository<User, Long> {
     User getOneByEmail(String email);
+    User getOneById(Long id);
 }
