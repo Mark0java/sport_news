bc05e9c160d8763d9229703a4b3a78322125bb18
Bohdantrush08 <32901504+Bohdantrush08@users.noreply.github.com>
2019-08-15 14:04:50 +0300

    Edited Repository

--- src/main/java/com/turtles/sport_news/repository/UserRepository.java
+++ src/main/java/com/turtles/sport_news/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.turtles.sport_news.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository  extends JpaRepository {
+@Repository
+public interface UserRepository  extends JpaRepository<User, Long> {
     User getOneByEmail(String email);
 }
