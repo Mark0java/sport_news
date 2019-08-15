bdd0c270ac9d0d5b278b422a9322b07cd65a71dd
BohdanMytnyk <bbmytnyk@gmail.com>
2019-08-15 14:30:17 +0300

    added UsereService, fixed UserRepository

--- src/main/java/com/turtles/sport_news/repository/UserRepository.java
+++ src/main/java/com/turtles/sport_news/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.turtles.sport_news.repository;
 
+import com.turtles.sport_news.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Repository;
 
 @Repository
