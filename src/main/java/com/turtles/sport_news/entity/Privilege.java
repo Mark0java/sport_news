97f958cc7894defaffb532f5332dfb7faeaa935b
BohdanMytnyk <bbmytnyk@gmail.com>
2019-08-16 14:12:48 +0300

    changed entities

--- src/main/java/com/turtles/sport_news/entity/Privilege.java
+++ src/main/java/com/turtles/sport_news/entity/Privilege.java
@@ -14,18 +14,6 @@ public class Privilege {
     @Column
     private String name;
 
-    public Privilege(String name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
